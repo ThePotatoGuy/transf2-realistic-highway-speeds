426dedf526b6f3ac31c8770176262f5c5521677b
potato <potato@desu.zone>
2020-01-08 00:09:49 -0600

    km

---
+++ thepotatoguy_realistic_highway_speeds_km_1/mod.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "CRITICAL",
+		name = _("Realistic Highway Speeds (KM/H)"),
+		description = _("Adds variants of the stock highways with specific speeds (KM/H only)\nRecommended to use this with my Faster Car Speeds mod."),
+		tags = { "Street" },
+		authors = {
+			{
+				name = "ThePotatoGuy",
+				role = "CREATOR",
+			},
+		},
+	}
+}
+end
